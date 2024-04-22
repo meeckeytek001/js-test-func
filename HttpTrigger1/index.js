5203b439dbaa4a124c31f33b93fafc603cf6f2db
meeckeytek001 <meeckeytek001@gmail.com>
2024-04-22 13:45:49 +0100

    updated

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,4 +1,6 @@
-const bcrypt = require("bcrypt");
+const bcryptjs = require("bcryptjs");
+const dotenv = require("dotenv")
+dotenv.config();
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -6,14 +8,14 @@ module.exports = async function (context, req) {
     let salt;
     let hashedPassword
     try {
-        salt = await bcrypt.genSalt(12);
-    hashedPassword = await bcrypt.hash(req.body.name, salt);
+        salt = await bcryptjs.genSalt(12);
+    hashedPassword = await bcryptjs.hash(req.body.name, salt);
     console.log("generated pass " + hashedPassword)
     } catch (error) {
         
     }
     const responseMessage = name
-        ? "Hello, " + name + " your password for function 1 is " + hashedPassword : "Please supply the required value";
+        ? "Hello, " + name + " your password for function 1 is " + hashedPassword + " env " + process.env.MYDIGIT : "Please supply the required value";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
